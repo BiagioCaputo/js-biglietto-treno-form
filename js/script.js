e1f6d71d9b1e402c325713b074f378f5f088cbe2
BiagioCaputo <scebiagio98@gmail.com>
2023-12-19 16:29:44 +0100

    milestone 1 terminato

---
+++ js/script.js
@@ -0,0 +1,41 @@
+console.log("JS OK");
+
+//creo le mie variabili
+
+const kms = document.getElementById("kms");
+const button = document.getElementById("button");
+const finalprice = document.getElementById("priceticket");
+
+
+
+//Attivo l'ascolto sul bottone 
+
+button.addEventListener("click", function(){
+
+    //recupero i dati dell'utente
+
+    const userKms = parseInt(kms.value);
+    const userAge = document.getElementById("age").value;
+    const price = userKms * 0.21;
+
+    console.log(userKms);
+    console.log(userAge);
+
+    if (userAge ==="maggiorenne"){
+        finalprice.innerText = price.toFixed(2);
+    }
+
+    else if (userAge ==="minorenne"){
+        const price20 = price - price * 20 / 100;
+        finalprice.innerText =  price20.toFixed(2);       
+    }
+
+    else if (userAge ==="over65"){
+        const price40 = price - price * 40 / 100;
+        finalprice.innerText = price40.toFixed(2); 
+    }
+
+        
+
+})
+
